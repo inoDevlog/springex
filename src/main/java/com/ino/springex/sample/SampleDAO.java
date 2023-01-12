ff96ad074b082f2ad670e992f0bfda40ea54329a
choiinho <ino0319@gmail.com>
2023-01-13 02:36:12 +0900

    4장. 스프링과 스프링 Web MVC ing...

--- src/main/java/com/ino/springex/sample/SampleDAO.java
+++ src/main/java/com/ino/springex/sample/SampleDAO.java
@@ -1,2 +1,7 @@
-package com.ino.springex.sample;public class SampleDAO {
+package com.ino.springex.sample;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SampleDAO {
 }
